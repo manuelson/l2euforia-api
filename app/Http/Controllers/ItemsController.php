cc99b2bf1dc5a8668c938d140e732b074a5f0999
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2022-08-31 13:23:33 +0200

    order by

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -29,7 +29,7 @@ class ItemsController extends Controller
             );
 
             $news = Items::Where('loc', $request->type)
-                ->Where('owner_id', $request->owner_id)->orderBy('enchant_level', 'DES')->get();
+                ->Where('owner_id', $request->owner_id)->get();
 
             return response()->json(['message' => $news, 'error' => false], 200);
         } catch (\Exception $e) {
